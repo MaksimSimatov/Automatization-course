58db436fdd2dbc1b99c1ef3fc9fa64d8e3e7ad91
MaksimSimatov <Maksim.Simatov@gmail.com>
2019-02-05 21:41:42 +0400

    change

---
+++ src/com/epam/maksim_simatov/java/lesson1/task1/ArraysProcessing.java
@@ -0,0 +1,8 @@
+package com.epam.maksim_simatov.java.lesson1.task1;
+
+public class ArraysProcessing {
+    public static void main(String[] args) {
+        System.out.println("First version");
+        System.out.println("Changing");
+    }
+}
